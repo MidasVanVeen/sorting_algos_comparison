18380d8a4bff49bd430350d62e640316a55f7368
MidasVanVeen <midasvanveen.email@gmail.com>
2022-04-21 23:42:43 +0200

    snd

--- main.c
+++ main.c
@@ -65,8 +65,7 @@ int main() {
 	printf("Average bubblesort times: ");
 	for (int i = 0; i < 4; i++) {
 		printf("%f, ", average_times_bubblesort[i]);
-	} printf("\n");
-	printf("Average mergesort times: ");
+
 	for (int i = 0; i < 4; i++) {
 		printf("%f, ", average_times_mergesort[i]);
 	} printf("\n");
@@ -110,19 +109,16 @@ void merge(int arr[], int l, int m, int r) {
     int i, j, k;
     int n1 = m - l + 1;
     int n2 = r - m;
-    /* create temp arrays */
     int L[n1], R[n2];
   
-    /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1 + j];
-  
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+
+    i = 0;
+    j = 0;
+    k = l;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
             arr[k] = L[i];
@@ -135,16 +131,12 @@ void merge(int arr[], int l, int m, int r) {
         k++;
     }
   
-    /* Copy the remaining elements of L[], if there
-    are any */
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
   
-    /* Copy the remaining elements of R[], if there
-    are any */
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -152,15 +144,10 @@ void merge(int arr[], int l, int m, int r) {
     }
 }
   
-/* l is for left index and r is right index of the
-sub-array of arr to be sorted */
 void mergeSort(int arr[], int l, int r) {
     if (l < r) {
-        // Same as (l+r)/2, but avoids overflow for
-        // large l and h
         int m = l + (r - l) / 2;
   
-        // Sort first and second halves
         mergeSort(arr, l, m);
         mergeSort(arr, m + 1, r);
   
